50c8e26ba439a43f084ba554e3cc16a1da7da0fa
Aman Shrivastav <amanshrivast21@gmail.com>
2025-05-09 18:21:34 +0530

    Create check-instances-state-stopped.py
    This will check your instances which are stopped in your account and give there Instances ID's as an output.

---
+++ check-instances-state-stopped.py
@@ -0,0 +1,25 @@
+## Check the instance state which are stopped in your accont and get the output of those instaces ID's
+
+import boto3
+def lambda_handler(event, context): # Create an EC2 client ec2 = boto3.client('ec2')
+
+# Describe instances
+response = ec2.describe_instances()
+
+# List to hold instance IDs
+instance_ids = []
+
+# Iterate over reservations and instances
+for reservation in response['Reservations']:
+    for instance in reservation['Instances']:
+        # Check the state of the instance
+        state = instance['State']['Name']
+        # Add instance ID to the list if the instance is stopped
+        if state == 'stopped':
+            instance_ids.append(instance['InstanceId'])
+
+# Return the list of instance IDs
+return {
+    'statusCode': 200,
+    'body': instance_ids
+}
